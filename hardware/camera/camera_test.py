e48d5070bfb6a5e1ec8646980a2b56bdba9cfed8
rpi3 <afakharany93@gmail.com>
2017-06-18 01:13:56 +0200

    adjust for rpi3

--- hardware/camera/camera_test.py
+++ hardware/camera/camera_test.py
@@ -1,9 +1,9 @@
 import sys
 import os
 import platform
-if platform.platform() == 'Linux-4.4.50-v7+-armv7l-with-debian-8.0':
+if platform.platform() == 'Linux-4.9.32-v7+-armv7l-with-debian-8.0':
     import subprocess
-    subprocess.call(" ln -s /usr/local/lib/python3.4/dist-packages/cv2.so cv2.so", shell=True)		        
+    subprocess.call(" ln -s /usr/local/lib/python3.4/dist-packages/cv2.cpython-34m.so cv2.so", shell=True)		        
 import cv2
 
 def set_res(cap, x,y):
@@ -12,7 +12,7 @@ def set_res(cap, x,y):
     return str(cap.get(3)),str(cap.get(4))
 
 # initialize the camera
-cam = cv2.VideoCapture(1)   # 0 -> index of camera
+cam = cv2.VideoCapture(0)   # 0 -> index of camera
 e,c = set_res(cam,1280,720)
 print(e,c)
 s, img = cam.read()
